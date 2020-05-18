8025735785713ffa340ed8fc5a2f79fe6a0fb675
github username <github email address>
2020-05-18 17:45:34 -0400

    Plan functions

--- clean-browse.js
+++ clean-browse.js
@@ -1,1 +1,43 @@
-clean-browse.js
+$(()=>{
+  console.log("clean-browse.js loaded ...")
+  onButtonClick();
+})
+
+function onButtonClick(){
+  $('span.action-button').on('click', (event) => {
+    if(event.currentTarget.dataset.id === 'remove'){
+      removeCode()
+    } else if (event.currentTarget.dataset.id === 'undo-last'){
+      console.log('execute undo-last code')
+ 
+    } else if (event.currentTarget.dataset.id === 'undo-all') {
+      console.log('execute undo-all code')
+ 
+    }
+  })
+}
+
+function removeCode() {
+  console.log('exeycute remove code')
+  
+  // readySelect
+    // page should light up, indicating we're ready for user select
+    // visible yellow border with msg: "CLICK ON THE ANNOYING THING"
+
+  // userSelect
+    // user click on something that is annoying
+    // we add the css selectors from the annoyingThings
+    // to our list of annoying things to hide
+
+  // addAnnoyingThing
+    // we call the hideAnnoyingThings function
+    // pass the new annoying thing selector to the lastThingClicked variable
+    // listen for click of undoLast button
+    // if clicked, remove the lastThingClicked from the
+
+  // removeLastAnnoyingThing
+    // pop()
+
+  // emptyAnnoyingThingsList
+  // refresh the browser
+}
