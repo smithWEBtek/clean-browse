939b150ac6e6e57fbc903ddf89fcc3ef765d37e1
github username <github email address>
2020-05-17 20:48:25 -0400

    Add action buttons

--- clean-browse.js
+++ clean-browse.js
@@ -1,1 +1,16 @@
-clean-browse.js
+$(()=>{
+  console.log("clean-browse.js loaded ...")
+  onButtonClick();
+})
+
+function onButtonClick(){
+  $('span.action-button').on('click', (event) => {
+    if(event.currentTarget.dataset.id === 'remove'){
+      console.log('remove clicked')
+    } else if (event.currentTarget.dataset.id === 'undo-last'){
+      console.log('undo-last clicked')
+    } else if (event.currentTarget.dataset.id === 'undo-all') {
+      console.log('undo-all clicked')
+    }
+  })
+}
